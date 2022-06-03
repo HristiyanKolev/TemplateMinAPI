1d84d45d82459957d7ec229272ec4b9e673a2f26
HristiyanKolev <hristiyan.kolev@gmail.com>
2022-06-03 14:14:32 +0300

    Added Packages

--- obj/Debug/net6.0/TemplateMinAPI.AssemblyInfo.cs
+++ obj/Debug/net6.0/TemplateMinAPI.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("49d2621b-3a2a-43e6-9609-af2b037895a6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateMinAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
