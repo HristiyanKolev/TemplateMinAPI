b4ca69ca68649c8e4ba68bac6475d4f0264ac180
HristiyanKolev <hristiyan.kolev@gmail.com>
2022-06-03 17:01:22 +0300

    Data migration and repository basics

---
+++ Data/CommandRepo.cs
@@ -0,0 +1,32 @@
+using TemplateMinAPI.Models;
+
+namespace TemplateMinAPI.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        public Task CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Command>> GetAllCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Command?> GetCommandById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
